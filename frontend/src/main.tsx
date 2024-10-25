563b9d712e5bdd70143d20f685b7ed88e54a0951
kemalege <cirpanliege@gmail.com>
2024-10-25 17:57:33 +0300

    Create base root and error-page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import './index.css'
-import App from './App.tsx'
+import Root from './routes/root';
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
