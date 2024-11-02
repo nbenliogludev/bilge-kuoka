f8b473225384b09bda66656c07d74c1bf4ecd38f
kemalege <cirpanliege@gmail.com>
2024-11-02 19:29:09 +0300

    format data by splitting header

--- frontend/src/routes/result-gemini/ResultPage.tsx
+++ frontend/src/routes/result-gemini/ResultPage.tsx
@@ -1,25 +1,31 @@
-import MarkdownRenderer from '@/utils/MarkDownRenderer';
-import { useLocation } from 'react-router-dom';
+import MarkdownRenderer from "@/utils/MarkDownRenderer";
+import { useLocation } from "react-router-dom";
 
 const ResultPage = () => {
   const location = useLocation();
-  const data = location.state ?? { data: 'Sonuç bulunamadı veya bir hata oluştu' };
+  const data = location.state ?? {
+    data: "Sonuç bulunamadı veya bir hata oluştu",
+  };
 
-  console.log('result page', data);
+  const [header, ...contentArray] = data.data[0].split("\n");
+
+  const content = contentArray.join("\n");
 
   return (
-    <div className='flex flex-col items-center mt-4'>
+    <div className="flex flex-col items-center mt-4">
       <div className="flex flex-col items-center text-center m-8">
-        <h1 className="font-bold text-3xl">Sonuç</h1>
+        <h1 className="font-bold text-3xl">
+          <MarkdownRenderer markdownText={header} />
+        </h1>
       </div>
 
       <div className="flex m-8 items-center">
         <div className="bg-white dark:bg-primary-foreground border-[1px] shadow-lg rounded-lg mx-auto p-6 space-y-6">
-           <MarkdownRenderer markdownText={data.data[0]} />
+          <MarkdownRenderer markdownText={content} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResultPage
+export default ResultPage;
