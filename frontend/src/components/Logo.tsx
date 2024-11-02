4a1a8e96b0d7e3a19b879833de8a704ed30202a2
kemalege <cirpanliege@gmail.com>
2024-11-02 19:32:01 +0300

    Resize logo, add dependencies

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="Logo" style={{ width: '150px', height: 'auto' }} />
+        <img src={logo} alt="Logo" style={{ width: '120px', height: 'auto' }} />
       </header>
     </div>
   );
