c54e61cec7468cfdd5b0e28e5cfa45e0e24c4b8a
kemalege <cirpanliege@gmail.com>
2024-11-02 15:33:48 +0300

    Update layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import Stats from "./routes/stats";
 import Root from "./routes/root";
 import SelectCategoriesPage from "./routes/select-categories/SelectionLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
@@ -21,15 +22,9 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "/select-categories",
+    path: "/start",
     element: <SelectCategoriesPage />,
     errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "stats",
-        element: <Stats />,
-      },
-    ],
   },
 ]);
 
@@ -38,7 +33,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <App>
+        <RouterProvider router={router} />
+      </App>
     </QueryClientProvider>
   </StrictMode>
 );
