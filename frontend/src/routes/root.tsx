5c25312cc4dc5b202a82d755e86d53fdb4aca5cb
kemalege <cirpanliege@gmail.com>
2024-11-02 15:34:10 +0300

    Update root

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -1,13 +1,10 @@
-import App from "@/App";
 import Layout from "./layout";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
-    <App>
-      <Layout>
-        <Outlet />
-      </Layout>
-    </App>
+    <Layout>
+      <Outlet />
+    </Layout>
   );
 }
